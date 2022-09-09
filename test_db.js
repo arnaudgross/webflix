09160d541021ab9e5d5263a371345bd79f6f68c6
Arnaud Gross <gross.arn@gmail.com>
2022-09-09 23:46:09 +0200

    remove db test file

--- test_db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect(
-    'mongodb+srv://cocomongo:Qij7UvEddjRhuPpS@cluster0.i85ml.mongodb.net/webflix?retryWrites=true&w=majority', 
-    {connectTimeoutMS : 3000, socketTimeoutMS: 20000, useNewUrlParser: true, useUnifiedTopology: true }
-);
-const db = mongoose.connection;
-db.once('open', () => {
-   console.log(`connexion OK !`);
-});
